798a8c77992685226dc1c9d7c610dd4ee8fea7cb
Chris Schuhmacher <chris@fandom.com>
2020-02-01 23:59:42 -0800

    Created title screen with cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.lightBlue[900],
+        backgroundColor: Colors.indigo.shade100,
         appBar: AppBar(
-          backgroundColor: Colors.deepOrange[900],
-          title: Center(
-            child: Text(
-              'Fitness Monkey',
-              style: TextStyle(color: Colors.yellow[600]),
-            ),
+          backgroundColor: Colors.indigo.shade700,
+          title: Text(
+            'Fitness Monkey',
           ),
         ),
         body: Center(
-          child: Image(
-            image: AssetImage('images/fm_app_icon.png'),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('images/fm_app_icon.png'),
+                  radius: 50.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Fitness Monkey',
+                  style: TextStyle(
+                    color: Colors.indigo.shade500,
+                    fontSize: 36.0,
+                    fontFamily: 'Bangers',
+                  ),
+                ),
+                Text(
+                  'Addiction Recovery through Physical Fitness',
+                  style: TextStyle(
+                    color: Colors.indigo.shade500,
+                    fontSize: 18.0,
+                    fontFamily: 'Source Sans Pro',
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.indigo.shade900,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 25.0,
+                    vertical: 10.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.local_hospital,
+                      color: Colors.deepOrange.shade500,
+                    ),
+                    title: Text(
+                      'Addiction Recovery',
+                      style: TextStyle(
+                        color: Colors.indigo.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 25.0,
+                    vertical: 10.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.motorcycle,
+                      color: Colors.deepOrange.shade500,
+                    ),
+                    title: Text(
+                      'Physical Fitness',
+                      style: TextStyle(
+                        color: Colors.indigo.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 25.0,
+                    vertical: 10.0,
+                  ),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.people,
+                      color: Colors.deepOrange.shade500,
+                    ),
+                    title: Text(
+                      'Community Support',
+                      style: TextStyle(
+                        color: Colors.indigo.shade900,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
-    ));
+    );
+  }
+}
