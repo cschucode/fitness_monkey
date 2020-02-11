24b1d2a802d93ddead00cbc7335993874a902ab2
Chris Schuhmacher <chris@fandom.com>
2020-02-11 08:40:46 -0800

    add pedometer package

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
+import 'package:pedometer/pedometer.dart';
 
 void main() => runApp(MyApp());
 
